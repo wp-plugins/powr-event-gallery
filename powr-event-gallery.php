5bea1e4d10b2d9827e41a52dc4aa49253a93669f
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-13 20:20:21 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-event-gallery/trunk@1180385 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-event-gallery.php
+++ powr-event-gallery.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Event Gallery
-     * @version 1.4.004
+     * @version 1.4.005
      */
     /*
     Plugin Name: POWr Event Gallery
     Plugin URI: http://www.powr.io
     Description: Showcase upcoming events in a beautiful Pinterest-style gallery!  Drop the widget anywhere in your theme. Or use the POWr icon in your WP text editor to add to a page or post. Edit on your live page by clicking the settings icon. More plugins and tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.4.004
+    Version: 1.4.005
     Author URI: http://www.powr.io
     */
 
